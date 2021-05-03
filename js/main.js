e3d97a4207c092968eb2afb2dfbf3807b1b7d773
florian <martinflorian2@gmail.com>
2021-05-03 11:43:56 +0200

    score array and replay function

--- js/main.js
+++ js/main.js
@@ -1,45 +1,65 @@
-const min = 0;
-let max = 100;
-let guessNumber = 0;
-let attempts = 0;
-let randomNumber = Math.round(Math.random()*max);
-console.log(randomNumber);
+function play(config) {
 
-const playGame = function() {
-    guessNumber = Number(prompt(`I have made a number wetween ${min} and ${max}, you have to find it.`));
-    if (randomNumber === guessNumber) {
-        attempts++;
-        alert(`you win, my number was ${randomNumber}. It took you ${attempts} attempt(s) to find it. Can you do better ?`);
+    config.attempts = 1;
+
+    // we give a value to the number to guess.
+    config.target = getrandomNumber(config.min, config.max);
+    console.log(config);
+
+    // we let the player define a first value for his guess.
+    config.guess = parseInt(prompt(`Find my number between ${config.min} and ${config.max}.`));
+
+    while (config.target !== config.guess) {
+        if (config.guess > config.target && config.guess < config.max) {
+            config.guess = parseInt(prompt(`My number is smaller than ${config.guess} fellow human, go on.`));
+            console.log('smaller');
+        }
+
+        else if (config.guess < config.target && config.guess > config.min) {
+            config.guess = parseInt(prompt(`My number is greater than ${config.guess} fellow human, go on.`));
+            console.log('greater');
+        }
+
+        else if (config.guess < config.min ) {
+            config.guess = parseInt(prompt(`Becareful fellow human, my number is between ${config.min} and ${config.max}. Try again.`));
+            console.log('less than min');
+        }
+
+        else if (config.guess > config.max) {
+            config.guess = parseInt(prompt(`Becareful fellow human, my number is between ${config.min} and ${config.max}. Try again.`));
+            console.log('more than max');
+        }
+
+        else {
+            config.guess = parseInt(prompt(`Your answer must be a number fellow human.`));
+            console.log('not a number');
+        }
+        config.attempts++;
+
     }
-    else if (randomNumber > guessNumber) {
-        attempts++;
-        alert(`No, my number is greater than ${guessNumber}`);
-        playGame();
+    config.score.push(config.attempts);
+    console.log(config.score);
+    let replay = confirm(`You win fellow human, my number was ${config.target}. It took you ${config.attempts} attempt(s) to find it. Would you like to play again ?`);
+    if (replay) {
+        play(config);
     }
-    else if (randomNumber < guessNumber) {
-        attempts++;
-        alert(`No, my number is samller than ${guessNumber}`);
-        playGame();
-    }
-    else if (guessNumber !== Number) {
-        attempts++;
-        alert(`Your answer must be a number.`);
-        playGame();
-    }
-    else if (guessNumber < min || guessNumber > max) {
-        attempts++;
-        alert(`Becareful, my number is between ${min} and ${max}.`);
-        playGame();
+    else {
+        console.log(config.score);
     }
 }
 
-playGame();
+ function getrandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 
-// let replay = function() {
-//     confirm(`do you want play again ?`);
-//     if (replay) {
-//         playGame();
-//     }
-// }
+}
 
-// replay();
+var game = {
+    min: 0,
+    max: 100,
+    attempts: 1,
+    target: null,
+    guess: null,
+    score: []
+}
+
+play(game);
